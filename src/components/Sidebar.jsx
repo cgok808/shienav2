f0d882358a571600106ada064a0f45add3226348
Chay <chaylin.gokan@gmail.com>
2022-10-22 21:04:34 -0400

    Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import {
+  FaSuitcase,
+  FaHome,
+  FaMusic,
+  FaVolumeUp,
+  FaMicrophone,
+  FaImages,
+  FaStar,
+  FaCommentAlt,
+} from "react-icons/fa";
+
+const SidebarLink = ({ icon, label }) => {
+  return (
+    <div className='flex font-bold transition-all ease-linear duration-300 pb-2 text-2xl hover:text-qWhite'>
+      <span className='mt-1'>{icon}</span>
+      <span className='ml-5 cursor-pointer hidden lg:block'>{label}</span>
+    </div>
+  );
+};
 
 const Sidebar = () => {
   return (
-    <nav className="h-full w-60 fixed top-0 left-0 bg-[#242025] overflow-x-hidden pt-5 flex flex-col items-center ">
-      <h1>AHR</h1>
+    <nav className='h-full fixed top-0 left-0 bg-qBlack overflow-x-hidden pt-5 flex flex-col items-center text-qPink transition-all duration-300 w-0 md:w-32 lg:w-60 ease-linear'>
+      <h1 className='lg:text-5xl font-bold mt-10 text-3xl transition-all duration-300 ease-linear'>AHR</h1>
+      <div className='h-3 lg:w-24 rounded-sm mt-2 mb-5 bg-qPink transition-all duration-300 ease-linear w-1/2'>
+        <div className='w-1/2 h-3 rounded-sm bg-qBlack border-2 border-qPink hidden lg:block'></div>
+      </div>
+      <div>
+        <SidebarLink icon={<FaHome />} label={"home"} />
+        <SidebarLink icon={<FaSuitcase />} label={"services"} />
+        <SidebarLink icon={<FaVolumeUp />} label={"demos"} />
+        <SidebarLink icon={<FaMicrophone />} label={"studio"} />
+        <SidebarLink icon={<FaImages />} label={"roles"} />
+        <SidebarLink icon={<FaStar />} label={"reviews"} />
+        <SidebarLink icon={<FaCommentAlt />} label={"contact"} />
+      </div>
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "qPink": "#dca69c",
+        "qWhite": "#fdf9ee",
+        "qBlack": "#242025"
+      },
+    },
   },
   plugins: [],
 };
