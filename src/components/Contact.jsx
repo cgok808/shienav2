e04f4a341e376537d67826bdff50fad0d5cb1c97
Chay <chaylin.gokan@gmail.com>
2022-11-12 07:57:13 -0500

    made contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import SubHeading from './SubHeading'
+import React from "react";
+import SubHeading from "./SubHeading";
 
 const Contact = () => {
   return (
-    <section className='bg-[#b58c90]' id="contact">
-      <SubHeading title={"contact"} subtitle={"LET'S TALK"} color={"text-qWhite"} />
+    <section className='bg-[#AD7F84]' id='contact'>
+      <SubHeading
+        title={"contact"}
+        subtitle={"LET'S TALK"}
+        color={"text-qWhite"}
+      />
+      <form className='mt-20 mb-20'>
+        <div className='flex justify-around'>
+          <input
+            type='text'
+            placeholder='Name'
+            className='w-[40%] p-5 bg-qWhite rounded-xl'
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            className='w-[40%] p-5 bg-qWhite rounded-xl'
+          />
+        </div>
+        <div className='flex flex-col items-center'>
+          <input
+            type='text'
+            placeholder='Subject'
+            className='w-[90%] p-5 bg-qWhite rounded-xl mt-3'
+          />
+          <textarea
+            type='text'
+            placeholder='Message'
+            className='w-[90%] p-5 pb-20 bg-qWhite rounded-xl mt-3'
+          />
+        </div>
+      </form>
+      <div className='flex justify-center'>
+        <button className='bg-[#B58C90] p-5 mb-36 rounded-xl w-[40%] font-extrabold text-2xl text-qWhite hover:bg-[#b99ea1] transition__ease'>
+          send ur shit
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
