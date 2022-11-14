ba87e488ce16e0f183488d468c0e1878b49495da
Chay <chaylin.gokan@gmail.com>
2022-11-14 08:59:05 -0500

    required form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,25 +37,30 @@ const Contact = () => {
       <form className='mt-20 mb-20' ref={form} onSubmit={sendEmail} id='form'>
         <div className='flex justify-around'>
           <input
+            required
             type='text'
             name='from_name'
             placeholder='Name'
             className='w-[40%] p-5 bg-qWhite rounded-xl'
           />
           <input
+            required
             type='email'
             placeholder='Email'
+            name='from_email'
             className='w-[40%] p-5 bg-qWhite rounded-xl'
           />
         </div>
         <div className='flex flex-col items-center'>
           <input
+            required
             type='text'
             name='subject'
             placeholder='Subject'
             className='w-[90%] p-5 bg-qWhite rounded-xl mt-3'
           />
           <textarea
+            required
             name='message'
             type='text'
             placeholder='Message &#13; &#10;ex. Hello there, I am someone who would love to inquire about comissioning you to have your super hot cool voice be featured in my project!'
