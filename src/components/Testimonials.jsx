d08901b047b167aad472d636d468c8230d6bc933
Chay <chaylin.gokan@gmail.com>
2023-01-12 19:23:35 -0500

    fixed major responsive flaws with testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -43,38 +43,40 @@ const Testimonials = () => {
       />
       <div className='mt-12 mb-12 flex flex-col items-center'>
         <div
-          className={`h-80 w-1/2 ${
+          className={`h-80 md:w-1/2 w-80 ${
             color ? "bg-[#664A58]" : "bg-[#E8CCB9]"
           } rounded-2xl flex justify-center items-center transition__ease `}
         >
+          <div
+            className={`${
+              color ? "text-qWhite" : "text-qBlack"
+            } flex justify-center items-center text-center w-[90%] md:w-[80%] h-full text-sm lg:text-base text-ellipsis overflow-hidden`}
+          >
+            {description}
+          </div>
+        </div>
+        <div className='flex justify-around'>
           <BsArrowLeftSquareFill
             onClick={prevPerson}
             className={`${
               color ? "text-[#322431]" : "text-[#DAB19E]"
-            } w-20 lg:w-16 h-20 lg:h-16 relative right-20 md:right-24 lg:right-28 transition__ease hover:scale-110 active:scale-90`}
+            } relative top-6 right-6 text-6xl transition__ease hover:scale-110 active:scale-90 md:block`}
           />
-          <p
-            className={`${
-              color ? "text-qWhite" : "text-qBlack"
-            } text-center w-[80%] text-lg`}
+          <div
+            className={`relative h-22 top-4 ${
+              color ? "bg-[#322431]" : "bg-[#DAB19E]"
+            } rounded-xl flex text-center items-center justify-center transition__ease`}
           >
-            {description}
-          </p>
+            <h6 className='text-qWhite font-bold text-xl md:text-2xl p-5'>
+              {name}
+            </h6>
+          </div>
           <BsArrowRightSquareFill
             onClick={nextPerson}
             className={`${
               color ? "text-[#322431]" : "text-[#DAB19E]"
-            } w-20 lg:w-16 h-20 lg:h-16 relative left-20 md:left-24 lg:left-28 transition__ease hover:scale-110 active:scale-90`}
+            } relative top-6 left-6 text-6xl transition__ease hover:scale-110 active:scale-90 md:block`}
           />
-        </div>
-        <div
-          className={`relative bottom-12 h-24 w-1/4 ${
-            color ? "bg-[#322431]" : "bg-[#DAB19E]"
-          } rounded-2xl flex text-center items-center justify-center transition__ease`}
-        >
-          <h6 className='text-qWhite font-bold md:truncate md:max-w-2xl'>
-            {name}
-          </h6>
         </div>
       </div>
     </section>
