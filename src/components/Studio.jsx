9f0bd9910b13703d21db215ff789dba95c54b24f
cgok808 <chaylin.gokan@gmail.com>
2023-03-06 17:37:59 -0800

    collapsible component fix

--- src/components/Studio.jsx
+++ src/components/Studio.jsx
@@ -1,35 +1,73 @@
 import React, { useState } from "react";
+import SubHeading from "./SubHeading";
 
-const Accordion = () => {
+const Accordion = ({ title, content }) => {
   const [open, setOpen] = useState(false);
 
   const toggle = () => {
     setOpen(!open);
-    
   };
   return (
-    <a className='w-[45%] bg-[#F4E2D7] rounded-lg mt-5 pt-3 px-3 pb-2 flex justify-between items-center border-b-black'>
-      <div className='font-extrabold text-2xl'>EQUIPMENT</div>
-      <button className='font-extrabold text-3xl' onClick={toggle}>
-        +
-      </button>
-      {open && (
-        <div>
-          <h3>hello there!</h3>
-        </div>
-      )}
+    <a className='md:w-[45%] bg-[#F4E2D7] rounded-lg mt-5 pt-3 px-3 pb-2  border-b-black'>
+      <div className='flex justify-between items-center'>
+        <div className='font-extrabold text-2xl'>{title}</div>
+        <button className='font-extrabold text-3xl' onClick={toggle}>
+          +
+        </button>
+      </div>
+
+      <div>
+        {open && (
+          <div>
+            <h3>{content}</h3>
+          </div>
+        )}
+      </div>
     </a>
   );
 };
 
 const Studio = () => {
+  const [equipments, setEquipments] = useState([
+    {
+      id: 1,
+      title: "Equipment",
+      content: "Lorem ipsum",
+    },
+    {
+      id: 2,
+      title: "Equiment #2",
+      content: "Lorem ipsum",
+    },
+    {
+      id: 3,
+      title: "Equiment #3",
+      content: "Lorem ipsum",
+    },
+    {
+      id: 4,
+      title: "Equiment #4",
+      content: "Lorem ipsum",
+    },
+  ]);
+
   return (
-    <section id='studio' className=' bg-[#654A59] p-10'>
-      <div className='flex justify-between items-center flex-wrap'>
-        <Accordion />
-        <Accordion />
-        <Accordion />
-        <Accordion />
+    <section id='studio' className=' bg-[#D9A09F] p-10'>
+      <SubHeading
+        title={"studio"}
+        color={"text-[#513947]"}
+        subtitle={"THE SPECS"}
+      />
+      <div className='md:flex justify-between items-center flex-wrap'>
+        {equipments.map((equipment) => {
+          return (
+            <Accordion
+              key={equipment.id}
+              title={equipment.title}
+              content={equipment.content}
+            />
+          );
+        })}
       </div>
     </section>
   );
